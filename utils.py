5d8f92d3c9705d38076d9f65ffb0f0950d59794c
Thiago Esterci Fernandes <thiago.esterci@engenharia.ufjf.br>
2022-03-16 13:29:14 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -133,3 +133,92 @@ class unite_files:
                 )
 
         del inadequate
+
+class db_reduction:
+
+    def __init__(self,input_folder,data_output_folder='iteration_data/', target_output_folder='iteration_target/',n_iterations = 33,maintained_percent=0.3):
+        
+        import glob
+
+        self.file_list = glob.glob(input_folder+'*')
+        self.file_list.sort()
+
+        self.input_folder = input_folder
+        self.target_output_folder = target_output_folder
+        self.data_output_folder = data_output_folder
+        self.maintained_percent = maintained_percent
+        self.n_iterations = n_iterations
+
+    def transform(self):
+
+        import numpy as np
+        from sklearn.model_selection import train_test_split
+
+        bar = ProgBar(len(self.file_list*self.n_iterations),"Reducing number of time series...")
+
+        for i in range(self.n_iterations):
+            for j,file in enumerate(self.file_list):
+
+                data = pickle.load(open(file, "rb"))
+
+                n_measures = int(np.max(data[:, 1]))
+                n_ids = len(np.unique(data[:,0]))
+
+                expanded_dimensions = data.reshape(n_ids, n_measures, 8)
+
+                aux,reduced_data = train_test_split(expanded_dimensions,test_size=self.maintained_percent,
+                                                    shuffle=False)
+                
+                n_ids = len(np.unique(reduced_data[:,0]))
+                
+                reduced_data = reduced_data.reshape(len(reduced_data)*n_measures, 8)
+
+                del data
+                del aux
+
+                if j < 12:
+
+                    if j == 0:
+
+                        it_data = reduced_data
+
+                        it_target = np.zeros(len(reduced_data))
+
+                    else:
+
+                        it_data = np.vstack((it_data,reduced_data))
+
+                        target = np.zeros(len(reduced_data))
+                        
+                        it_target = np.hstack((it_target,target))
+
+
+                elif j >= 12 and j < 14:
+
+                    it_data = np.vstack((it_data,reduced_data))
+
+                    target = np.ones(len(reduced_data))
+                    
+                    it_target = np.hstack((it_target,target))
+
+                elif j >= 14:
+
+                    it_data = np.vstack((it_data,reduced_data))
+
+                    target = np.ones(len(reduced_data)) + 1
+                    
+                    it_target = np.hstack((it_target,target))
+
+                bar.update()
+
+            pickle.dump(it_data,
+                        open(self.data_output_folder + "data_mainteined_percent__{}__it__{}.pkl".format(
+                            self.maintained_percent,i
+                            ), "wb"), 
+                            )
+
+            pickle.dump(it_target,
+                        open(self.target_output_folder + "target_mainteined_percent__{}__it__{}.pkl".format(
+                            self.maintained_percent,i
+                            ), "wb"), 
+                            )
